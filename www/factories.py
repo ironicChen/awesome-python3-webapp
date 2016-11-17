d308415c3d5fe9091a0fd67140ea792cda6ee747
misaki <ironic_chen@foxmail.com>
2016-11-17 16:06:46 +0800

    day-05

---
+++ www/factories.py
@@ -0,0 +1,86 @@
+import logging;
+
+logging.basicConfig(level=logging.INFO)
+
+import json
+from aiohttp import web
+from urllib import parse
+
+
+async def logger_factory(app, handler):
+    async def logger(request):
+        logging.info('Request: %s %s' % (request.method, request.path))
+        # await asyncio.sleep(0.3)
+        return (await handler(request))
+
+    return logger
+
+
+async def data_factory(app, handler):
+    async def parse_data(request):
+        logging.info('data_factory...')
+        if request.method in ('POST', 'PUT'):
+            if not request.content_type:
+                return web.HTTPBadRequest(text='Missing Content-Type.')
+            content_type = request.content_type.lower()
+            if content_type.startswith('application/json'):
+                request.__data__ = await request.json()
+                if not isinstance(request.__data__, dict):
+                    return web.HTTPBadRequest(text='JSON body must be object.')
+                logging.info('request json: %s' % request.__data__)
+            elif content_type.startswith(('application/x-www-form-urlencoded', 'multipart/form-data')):
+                params = await request.post()
+                request.__data__ = dict(**params)
+                logging.info('request form: %s' % request.__data__)
+            else:
+                return web.HTTPBadRequest(text='Unsupported Content-Type: %s' % content_type)
+        elif request.method == 'GET':
+            qs = request.query_string
+            request.__data__ = {k: v[0] for k, v in parse.parse_qs(qs, True).items()}
+            logging.info('request query: %s' % request.__data__)
+        else:
+            request.__data__ = dict()
+        return await handler(request)
+
+    return parse_data
+
+
+async def response_factory(app, handler):
+    async def response(request):
+        logging.info('Response handler...')
+        r = await handler(request)
+        if isinstance(r, web.StreamResponse):
+            return r
+        if isinstance(r, bytes):
+            resp = web.Response(body=r)
+            resp.content_type = 'application/octet-stream'
+            return resp
+        if isinstance(r, str):
+            if r.startswith('redirect:'):
+                return web.HTTPFound(r[9:])
+            resp = web.Response(body=r.encode('utf-8'))
+            resp.content_type = 'text/html;charset=utf-8'
+            return resp
+        if isinstance(r, dict):
+            template = r.get('__template__')
+            if template is None:
+                resp = web.Response(
+                    body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+                resp.content_type = 'application/json;charset=utf-8'
+                return resp
+            else:
+                resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+                resp.content_type = 'text/html;charset=utf-8'
+                return resp
+        if isinstance(r, int) and r >= 100 and r < 600:
+            return web.Response(r)
+        if isinstance(r, tuple) and len(r) == 2:
+            t, m = r
+            if isinstance(t, int) and t >= 100 and t < 600:
+                return web.Response(t, str(m))
+        # default:
+        resp = web.Response(body=str(r).encode('utf-8'))
+        resp.content_type = 'text/plain;charset=utf-8'
+        return resp
+
+    return response
