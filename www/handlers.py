408307d0a5ebc2d279cc0e09a1fd9074a63dd58e
misaki <ironic_chen@foxmail.com>
2016-11-21 13:58:14 +0800

    day-08

--- www/handlers.py
+++ www/handlers.py
@@ -58,3 +58,14 @@ async def cookie2user(cookie_str):
     except Exception as e:
         logging.exception(e)
         return None
+
+
+def user2cookie(user, max_age):
+    """
+    Generate cookie str by user.
+    """
+    # build cookie string by: id-expires-sha1
+    expires = str(int(time.time() + max_age))
+    s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
+    L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+    return '-'.join(L)
